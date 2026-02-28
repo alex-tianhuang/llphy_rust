f6c517d543427f7b19729cb9db3b8a2d532198d8
alex-dsci <alex-dsci@github.com>
2026-02-28 13:13:32 -0500

    docs

--- rust/llphy_lib-260227/src/fasta.rs
+++ rust/llphy_lib-260227/src/fasta.rs
@@ -1,7 +1,17 @@
+//! Module defining [`parse_fasta_entries`].
 use crate::datatypes::{Aminoacid, FastaEntry, aa_canonical_str};
 use bumpalo::{Bump, collections::Vec};
 use std::mem::{self, ManuallyDrop};
 
+/// Parse a buffer with FASTA-formatted sequences
+/// into a vector of [`FastaEntry`] structs.
+/// 
+/// IO
+/// --
+/// This function logs to stderr when it encounters:
+/// - Headers that are non-UTF8
+/// - Sequences that contain non aminoacid (lowercase or uppercase) characters
+/// - Sequences that are empty (after removing all non aminoacid characters)
 pub fn parse_fasta_entries<'a>(bytes: Vec<'a, u8>, arena: &'a Bump) -> Vec<'a, FastaEntry<'a>> {
     let mut slice = leak_vec(bytes);
     let mut entries = Vec::new_in(arena);
@@ -44,19 +54,24 @@ pub fn parse_fasta_entries<'a>(bytes: Vec<'a, u8>, arena: &'a Bump) -> Vec<'a, F
             }
         }
         let sequence: &aa_canonical_str;
-        match parse_aastr(sequence_slice) {
+        match parse_sequence(sequence_slice) {
             ParseResult::Ok(s) => {
                 sequence = s;
             }
-            ParseResult::Warn(s) => {
+            ParseResult::WarnInvalidBytes(s) => {
                 eprintln!(
-                    "non aminoacid bytes removed while parsing this entry: {}",
-                    header
+                    "sequence was modified, non aminoacid bytes removed while parsing this entry: {}\nmodified sequence: {}",
+                    header,
+                    s.as_str(),
                 );
                 sequence = s;
             }
             ParseResult::Empty => {
                 eprintln!("could not parse entry (empty sequence): {}", header);
+                continue;
+            },
+            ParseResult::EmptyAndInvalidBytes => {
+                eprintln!("could not parse entry (all invalid bytes): {}", header);
                 continue;
             }
         }
@@ -64,11 +79,14 @@ pub fn parse_fasta_entries<'a>(bytes: Vec<'a, u8>, arena: &'a Bump) -> Vec<'a, F
     }
     entries
 }
+/// Find the index of the next newline from the beginning of this slice.
 fn end_of_current_header(bytes: &[u8]) -> Option<usize> {
     let b = bytes.iter().find(|b| **b == b'\n')?;
     let offset = (b as *const u8 as usize) - (bytes.as_ptr() as usize);
     Some(offset)
 }
+/// Find the index of the next newline followed by a `>` character
+/// from the beginning of this slice.
 fn next_start_of_header(bytes: &[u8]) -> Option<usize> {
     for pair in bytes.windows(2) {
         if pair == &[b'\n', b'>'] {
@@ -78,12 +96,25 @@ fn next_start_of_header(bytes: &[u8]) -> Option<usize> {
     }
     None
 }
-pub enum ParseResult<'a> {
+/// All possible cases returned by [`parse_sequence`].
+enum ParseResult<'a> {
+    /// The sequence is non-empty and does not merit any warning.
     Ok(&'a aa_canonical_str),
-    Warn(&'a aa_canonical_str),
+    /// The sequence contained non-aminoacid and non-whitespace bytes,
+    /// but after removing those it yielded a non-empty sequence.
+    WarnInvalidBytes(&'a aa_canonical_str),
+    /// The sequence contained no characters or only whitespace.
     Empty,
+    /// The sequence contained characters but none of them were
+    /// convertible to aminoacids.
+    EmptyAndInvalidBytes
 }
-fn parse_aastr(slice: &mut [u8]) -> ParseResult<'_> {
+/// Parse an [`aa_canonical_str`] from the given slice of bytes.
+/// 
+/// Parsing is done loosely, converting lowercase to uppercase and
+/// removing unexpected characters rather than failing outright.
+/// It will only fail if the sequence does not contain any aminoacids.
+fn parse_sequence(slice: &mut [u8]) -> ParseResult<'_> {
     let mut non_aa_start_idx = None;
     let mut warn = false;
     if slice.is_empty() {
@@ -91,14 +122,14 @@ fn parse_aastr(slice: &mut [u8]) -> ParseResult<'_> {
     }
     for i in 0..slice.len() {
         let b = slice[i];
-        if let Ok(_) = Aminoacid::try_from(b) {
+        if Aminoacid::try_from(b).is_ok() {
             continue;
         }
-        if let Ok(_) = Aminoacid::try_from(b.to_ascii_uppercase()) {
+        if Aminoacid::try_from(b.to_ascii_uppercase()).is_ok() {
             slice[i] = b.to_ascii_uppercase();
             continue;
         };
-        non_aa_start_idx = Some(i + 1);
+        non_aa_start_idx = Some(i);
         warn = should_warn(b);
         break;
     }
@@ -108,29 +139,36 @@ fn parse_aastr(slice: &mut [u8]) -> ParseResult<'_> {
         //         in the loop above
         unsafe { return ParseResult::Ok(aa_canonical_str::from_bytes_unchecked(slice)) }
     };
-    let mut shift = 1;
-    for i in start..slice.len() {
-        let b = slice[i];
-        if let Ok(_) = Aminoacid::try_from(b.to_ascii_uppercase()) {
-            slice[i - shift] = b.to_ascii_uppercase();
+    let mut len = start;
+    let mut cursor = start;
+    loop {
+        cursor += 1;
+        let Some(&b) = slice.get(cursor) else { break };
+        if Aminoacid::try_from(b.to_ascii_uppercase()).is_err() {
+            warn |= should_warn(b);
             continue;
         };
-        shift += 1;
-        warn |= should_warn(b);
+        slice[len] = b.to_ascii_uppercase();
+        len += 1;
     }
-    if slice.len() == shift {
-        return ParseResult::Empty;
+    if len == 0 {
+        if warn {
+            return ParseResult::EmptyAndInvalidBytes;
+        } else {
+            return ParseResult::Empty
+        }
     }
-
-    let len = slice.len() - shift;
     // SAFETY: safe for the same reason `Vec::retain` is safe.
     let s = unsafe { aa_canonical_str::from_bytes_unchecked(&slice[..len]) };
     if warn {
-        ParseResult::Warn(s)
+        ParseResult::WarnInvalidBytes(s)
     } else {
         ParseResult::Ok(s)
     }
 }
+/// Checks whether this byte merits a warning for this sequence.
+/// 
+/// Right now I ignore whitespace characters.
 fn should_warn(b: u8) -> bool {
     !b.is_ascii_whitespace()
 }
