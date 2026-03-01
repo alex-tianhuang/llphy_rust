3dd138e26e81886d46b7edaf82ce07bd7094ace6
alex-dsci <alex-dsci@github.com>
2026-02-28 21:01:29 -0500

    docs

--- rust/llphy_lib-260227/src/main.rs
+++ rust/llphy_lib-260227/src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::Error;
 use clap::Parser;
-use llphyscore::{Args, run_fasta_scorer};
+use llphyscore::{Args, bin_main};
 
+/// See [`llphyscore::bin_main`].
 fn main() -> Result<(), Error> {
     let args = Args::try_parse()?;
-    run_fasta_scorer(args)
+    bin_main(args)
 }
