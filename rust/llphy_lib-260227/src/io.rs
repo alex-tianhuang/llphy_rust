b16b82aaaa1fe94c3a785242579f838afb79bf00
alex-dsci <alex-dsci@github.com>
2026-03-02 14:55:07 -0500

    add read_file_into_global

--- rust/llphy_lib-260227/src/io.rs
+++ rust/llphy_lib-260227/src/io.rs
@@ -8,6 +8,21 @@ use std::{
     path::Path,
 };
 
+/// Read the file at `path` into a byte vector
+/// managed by the global allocator.
+/// 
+/// For non-persistent (and large!) files.
+pub fn read_file_into_global(path: &Path) -> Result<std::vec::Vec<u8>, Error>{
+    let mut file = File::open(path)?;
+    let filesize = file.metadata()?.len() as usize;
+    let mut buf = std::vec::Vec::with_capacity(filesize);
+    unsafe {
+        buf.set_len(filesize);
+    }
+    file.read_exact(&mut buf[..])?;
+    ensure_file_at_eof(file, buf.len() as u64)?;
+    Ok(buf)
+}
 /// Read the file at `path` into the given memory arena
 /// as a vector of bytes.
 pub fn read_file<'a>(path: &Path, arena: &'a Bump) -> Result<Vec<'a, u8>, Error> {
--- rust/llphy_lib-260227/src/load_legacy/mod.rs
+++ rust/llphy_lib-260227/src/load_legacy/mod.rs
@@ -1,18 +1,13 @@
 use anyhow::Error;
-use std::{
-    collections::HashMap, fs::File, io::{Cursor, Read}, path::{Path, PathBuf}
-};
+use std::path::Path;
 mod pdb_statistics;
+mod reference_g2w_scores;
 pub use pdb_statistics::load_legacy_pdb_statistics;
+pub use reference_g2w_scores::load_legacy_reference_g2w_scores;
+
+use crate::io::read_file_into_global;
 fn read_archive_file(filepath: &Path) -> Result<Vec<u8>, Error> {
     const DEV_ARCHIVE_ROOT: &'static str = env!("LEGACY_ARCHIVE_DIR");
     let filepath = Path::new(DEV_ARCHIVE_ROOT).join(filepath);
-    let mut file = File::open(filepath)?;
-    let filesize = file.metadata()?.len() as usize;
-    let mut bytes = Vec::with_capacity(filesize);
-    unsafe {
-        bytes.set_len(filesize);
-    }
-    file.read_exact(&mut bytes[..])?;
-    Ok(bytes)
+    read_file_into_global(&filepath)
 }
