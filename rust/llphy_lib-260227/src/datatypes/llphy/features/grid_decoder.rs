34d98da55f99a8a0919d2f909e0a0086eafb0787
alex-dsci <alex-dsci@github.com>
2026-03-04 03:54:58 -0500

    Add separate grid decoder mod

---
+++ rust/llphy_lib-260227/src/datatypes/llphy/features/grid_decoder.rs
@@ -0,0 +1,20 @@
+//! Module defining [`GridDecoder`].
+use crate::datatypes::{AAMap, llphy::features::thresholds::Thresholds};
+
+/// Turns a [`GridScore`](crate::datatypes::GridScore) into
+/// a sequence-level numeric feature.
+pub struct GridDecoder {
+    pub sign: i8,
+    pub thresholds: Thresholds,
+}
+impl GridDecoder {
+    /// Compute the sequence level feature
+    /// from the given grid scores.
+    pub fn compute(&self, grid_score: &AAMap<&[f64]>) -> i64 {
+        let mut score = 0;
+        for (t, &subarr) in self.thresholds.values().zip(grid_score.values()) {
+            score += t.score_sites(subarr)
+        }
+        score * self.sign as i64
+    }
+}
