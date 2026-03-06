3f5095f77ceee7d7c46d32cbcff2e9fd443f41cb
alex-dsci <alex-dsci@github.com>
2026-03-06 01:02:58 -0500

    add ser/de for ReferenceFeatureMatrix

--- rust/llphy_lib-260227/src/datatypes/llphy/features.rs
+++ rust/llphy_lib-260227/src/datatypes/llphy/features.rs
@@ -1,9 +1,11 @@
 //! Datatypes for storing biophysical feature values
 //! associated to sequence(s).
-
-use std::slice::from_raw_parts_mut;
-use std::fmt::Write;
+use anyhow::Error;
+use borsh::{BorshDeserialize, BorshSerialize};
+use bumpalo::Bump;
 use bumpalo::collections::String;
+use std::fmt::Write;
+use std::slice::from_raw_parts_mut;
 
 /// Feature values for lots of sequences,
 /// along with associated feature names.
@@ -67,18 +69,61 @@ impl<'a> FeatureMatrix<'a, i64> {
         }
     }
 }
+impl<'a> ReferenceFeatureMatrix<'a> {
+    /// Morally equivalent to implement deserialize,
+    /// but with a memory arena to hold the [`Self::data`] field.
+    pub fn deserialize(buf: &mut &[u8], arena: &'a Bump) -> Result<Self, Error> {
+        let feature_names_len = <usize>::deserialize(buf)?;
+        let num_ref_seqs = <usize>::deserialize(buf)?;
+        let row_len = feature_names_len + 1;
+        let feature_names = &*arena.alloc_slice_try_fill_with(feature_names_len, |_| {
+            let feat_name_len = <usize>::deserialize(buf)?;
+            let feat_name = buf.get(..feat_name_len).ok_or_else(|| {
+                Error::msg(format!(
+                    "expected {} more bytes for string, but reached EOF",
+                    feat_name_len
+                ))
+            })?;
+            let feat_name_stable = &*arena.alloc_str(str::from_utf8(feat_name)?);
+            *buf = &buf[feat_name_len..];
+            <Result<_, Error>>::Ok(feat_name_stable)
+        })?;
+        let data =
+            arena.alloc_slice_try_fill_with(row_len * num_ref_seqs, |_| <i64>::deserialize(buf))?;
+        Ok(Self {
+            feature_names,
+            num_ref_seqs,
+            data,
+        })
+    }
+}
+impl BorshSerialize for ReferenceFeatureMatrix<'_> {
+    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
+        self.feature_names.len().serialize(writer)?;
+        self.num_ref_seqs.serialize(writer)?;
+        for feat_name in self.feature_names {
+            feat_name.len().serialize(writer)?;
+            writer.write_all(feat_name.as_bytes())?;
+        }
+        for slot in self.data.iter() {
+            slot.serialize(writer)?;
+        }
+        Ok(())
+    }
+}
 impl PostProcessedFeatureMatrix<'_> {
     /// Get feature names associated to this feature matrix.
     pub fn feature_names(&self) -> &[&str] {
-        let (Self::Raw(FeatureMatrix { feature_names, .. }) | Self::Processed(FeatureMatrix { feature_names, .. })) = *self;
+        let (Self::Raw(FeatureMatrix { feature_names, .. })
+        | Self::Processed(FeatureMatrix { feature_names, .. })) = *self;
         feature_names
     }
     /// Equivalent to `self.data.len()`
     /// if the enum was not in the way.
     fn data_len(&self) -> usize {
         match self {
-            Self::Raw(FeatureMatrix { data , .. }) => data.len(),
-            Self::Processed(FeatureMatrix { data, .. }) => data.len()
+            Self::Raw(FeatureMatrix { data, .. }) => data.len(),
+            Self::Processed(FeatureMatrix { data, .. }) => data.len(),
         }
     }
     /// One more than the number of feature names.
@@ -92,14 +137,18 @@ impl PostProcessedFeatureMatrix<'_> {
         let num_sequences = self.data_len() / self.row_len();
         debug_assert_eq!(self.data_len() % self.row_len(), 0);
         (0..num_sequences).map(move |i| match self {
-            Self::Raw(FeatureMatrix { data , .. }) => PostProcessedFeatureVector::Raw(&data[i * self.row_len()..][..self.row_len()]),
-            Self::Processed(FeatureMatrix { data, .. }) => PostProcessedFeatureVector::Processed(&data[i * self.row_len()..][..self.row_len()])
+            Self::Raw(FeatureMatrix { data, .. }) => {
+                PostProcessedFeatureVector::Raw(&data[i * self.row_len()..][..self.row_len()])
+            }
+            Self::Processed(FeatureMatrix { data, .. }) => {
+                PostProcessedFeatureVector::Processed(&data[i * self.row_len()..][..self.row_len()])
+            }
         })
     }
 }
 impl<'a> PostProcessedFeatureVector<'a> {
     /// Utility method for the [`crate::output`] module.
-    /// 
+    ///
     /// Write the values in this vector into a big comma separated list.
     pub fn format_comma_separated_into(&self, fmt: &mut String<'_>) {
         match *self {
