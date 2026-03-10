f484f279a69cfde8c3de8920f53ca52d102fa8f9
alex-dsci <alex-dsci@github.com>
2026-03-09 23:19:10 -0400

    lint docs

--- rust/llphyscore_core/src/datatypes/mod.rs
+++ rust/llphyscore_core/src/datatypes/mod.rs
@@ -1,7 +1,12 @@
+//! Module of datatypes, their serialization/deserialization methods,
+//! and some basic computations/conversions that can be performed on them.
+//! 
+//! [`sequences`] for sequence-related datatypes.
+//! [`llphy`] for LLPhyScore-specific datatypes.
 mod sequences;
-pub(crate) use sequences::{AAMap, AMINOACIDS, Aminoacid, FastaEntry, aa_canonical_str, AAIndex};
+pub use sequences::{AAMap, AMINOACIDS, Aminoacid, FastaEntry, aa_canonical_str, AAIndex};
 mod llphy;
-pub(crate) use llphy::{
+pub use llphy::{
     FeatureMatrix, MAX_XMER, PostProcessedFeatureMatrix, ReferenceFeatureMatrix,
-    DEFAULT_FEATURES, find_pair_and_features_from_one_feature_name, PAIR_NAMES_AND_FEATURE_NAMES
+    DEFAULT_FEATURES, GridScore, GridDecoder, GridScorer, PAIR_NAMES_AND_FEATURE_NAMES
 };
