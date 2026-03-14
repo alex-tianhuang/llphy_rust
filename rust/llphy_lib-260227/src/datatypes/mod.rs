e1a9f6032ec96cb895f7b114f95b25a9a70a697e
alex-dsci <alex-dsci@github.com>
2026-03-14 14:39:50 -0400

    remove old llphy_lib-260227

--- rust/llphy_lib-260227/src/datatypes/mod.rs
+++
@@ -1,7 +0,0 @@
-mod sequences;
-pub(crate) use sequences::{AAMap, AMINOACIDS, Aminoacid, FastaEntry, aa_canonical_str, AAIndex};
-mod llphy;
-pub(crate) use llphy::{
-    FeatureMatrix, MAX_XMER, ModelTrainingBase, PostProcessedFeatureMatrix, ReferenceFeatureMatrix,
-    ScoreType, DEFAULT_FEATURES, find_pair_and_features_from_one_feature_name, PAIR_NAMES_AND_FEATURE_NAMES
-};
--- rust/llphy_lib-260227/src/featurizer/grid_scorer/simd/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Module defining substructures of [`super::GridScorer`]
-//! using `#[portable_simd]`.
-pub use avg_sdev_db::AvgSdevDBEntry;
-pub use pair_freq_db::{PairFreqDBEntry, PairFreqEntrySum};
-pub use z_grid_db::{ZGridDBEntry, ZGridSubtable, ZGridEntrySum};
-mod z_grid_db;
-mod avg_sdev_db;
-mod pair_freq_db;
